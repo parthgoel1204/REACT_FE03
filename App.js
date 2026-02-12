d49985e88c91a226afd20b6a74d659da2b2db0ea
Parth Goel <parthgoel1204@gmail.com>
2026-02-12 19:43:05 +0530

    feat : scripts , react element & react component

--- App.js
+++ App.js
@@ -1,29 +1,49 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const heading = React.createElement("h1", {id : "heading"}, "Hello World from React!");
+// const heading = React.createElement("h1", {id : "heading"}, "Hello World from React!");
 //  the second argument in the createELement function is an object which will be used to give the attributes to the tags.
 
 
-const parent =  React.createElement("div",
-    {id: "parent"},
-   [ React.createElement("div" , 
-        {id: "child"},
-        [
-            React.createElement("h1", {}, "I'm an h1 tag!"),
-            React.createElement("h2", {}, "I'm an h2 tag!")
-        ]
-    ),React.createElement("div" , 
-        {id: "child2"},
-        [
-            React.createElement("h1", {}, "I'm an h1 tag!"),
-            React.createElement("h2", {}, "I'm an h2 tag!")
-        ]
-    )
-    ]
-);
+// const parent =  React.createElement("div",
+//     {id: "parent"},
+//    [ React.createElement("div" , 
+//         {id: "child"},
+//         [
+//             React.createElement("h1", {}, "I'm an h1 tag!"),
+//             React.createElement("h2", {}, "I'm an h2 tag!")
+//         ]
+//     ),React.createElement("div" , 
+//         {id: "child2"},
+//         [
+//             React.createElement("h1", {}, "I'm an h1 tag!"),
+//             React.createElement("h2", {}, "I'm an h2 tag!")
+//         ]
+//     )
+//     ]
+// );
 
-// for making the above structure clean we have #"JSX"#
+// // for making the above structure clean we have #"JSX"#
 const root = createRoot(document.getElementById("root"));
-// root.render(heading);
-root.render(parent);
+// // root.render(heading);
+// root.render(parent);
+const jsxHeading = <h1 id="heading">This is JSX </h1>; //this is not a valid Javascript
+// The above JSX code is converted to React.createELement() and then to JS object before rendering onto the DOM as HTML element.
+// console.log(jsxHeading);
+
+// root.render(jsxHeading)
+
+// REACT FUNCTIONAL COMPONENT
+const Title = () => (
+    <h1> This is a title component which is inside a heading component. </h1>
+)
+const HeadingComponent = () => (
+
+    <div>
+    <Title/>
+    <h1>This is a React Component!</h1>
+    </div>
+)
+
+const HeadingComponent2 = () => <h1>This is a React Component!</h1>
+root.render(<HeadingComponent/>)

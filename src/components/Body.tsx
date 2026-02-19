0b6c1e8653d2234b8218ea0de529721f8585f53d
Parth Goel <parthgoel1204@gmail.com>
2026-02-19 23:43:48 +0530

    feat: dynamic data fetching , useEffect hook, Shimmer UI

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,62 +1,83 @@
 import RestaurantCard from "./RestaurantCard";
-import {useState} from "react";
+import {useState , useEffect} from "react";
 import resList from "../utils/mockData";
+import Shimmer from "./Shimmer";
 const Body = ()=> {
-    let [listOfRestaurants, setListOfRestaurants] = useState([
-        {
-            "info": {
-                "id": "605415",
-                "name": "Dum Safar Biryani",
-                "cloudinaryImageId": "l9pbure8tbootzc7utmn",
-                 "costForTwo": "₹500 for two",
-                "cuisines": [
-                    "Biryani",
-                    "Kebabs",
-                    "North Indian",
-                    "Barbecue"
-                ],
-                "avgRating": 4,
-                "deliveryTime": 43,
+    // let [listOfRestaurants, setListOfRestaurants] = useState([
+    //     {
+    //         "info": {
+    //             "id": "605415",
+    //             "name": "Dum Safar Biryani",
+    //             "cloudinaryImageId": "l9pbure8tbootzc7utmn",
+    //              "costForTwo": "₹500 for two",
+    //             "cuisines": [
+    //                 "Biryani",
+    //                 "Kebabs",
+    //                 "North Indian",
+    //                 "Barbecue"
+    //             ],
+    //             "avgRating": 4,
+    //             "deliveryTime": 43,
             
-            }
-        },
-        {
-            "info": {
-                "id": "605416",
-                "name": "Dum Safar Biryani",
-                "cloudinaryImageId": "l9pbure8tbootzc7utmn",
-                 "costForTwo": "₹500 for two",
-                "cuisines": [
-                    "Biryani",
-                    "Kebabs",
-                    "North Indian",
-                    "Barbecue"
-                ],
-                "avgRating": 3.9,
-                "deliveryTime": 43,
+    //         }
+    //     },
+    //     {
+    //         "info": {
+    //             "id": "605416",
+    //             "name": "Dum Safar Biryani",
+    //             "cloudinaryImageId": "l9pbure8tbootzc7utmn",
+    //              "costForTwo": "₹500 for two",
+    //             "cuisines": [
+    //                 "Biryani",
+    //                 "Kebabs",
+    //                 "North Indian",
+    //                 "Barbecue"
+    //             ],
+    //             "avgRating": 3.9,
+    //             "deliveryTime": 43,
             
-            }
-        },
-        {
-            "info": {
-                "id": "605417",
-                "name": "Dum Safar Biryani",
-                "cloudinaryImageId": "l9pbure8tbootzc7utmn",
-                 "costForTwo": "₹500 for two",
-                "cuisines": [
-                    "Biryani",
-                    "Kebabs",
-                    "North Indian",
-                    "Barbecue"
-                ],
-                "avgRating": 4.6,
-                "deliveryTime": 43,
+    //         }
+    //     },
+    //     {
+    //         "info": {
+    //             "id": "605417",
+    //             "name": "Dum Safar Biryani",
+    //             "cloudinaryImageId": "l9pbure8tbootzc7utmn",
+    //              "costForTwo": "₹500 for two",
+    //             "cuisines": [
+    //                 "Biryani",
+    //                 "Kebabs",
+    //                 "North Indian",
+    //                 "Barbecue"
+    //             ],
+    //             "avgRating": 4.6,
+    //             "deliveryTime": 43,
             
-            }
-        }
-    ]);
-    // let [listOfRestaurants,setListOfRestaurants] = useState([resList]);
+    //         }
+    //     }
+    // ]);
 
+    let [listOfRestaurants,setListOfRestaurants] = useState([]);
+
+    useEffect(()=> {
+        fetchData();
+    },[]);
+
+    const fetchData = async () => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.719028&lng=77.077506&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+
+        const json = await data.json();
+
+        console.log(json);
+        // OPTIONAL CHAINING
+        setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+    // CONDITIONAL RENDERING
+    if(listOfRestaurants.length === 0){
+        return <Shimmer/>
+    }
     return(
         <div className="body">
             {/* <div className="search">Search</div> */}

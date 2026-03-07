ab568fa784bb2d75eecdbaa7ed6ea8a8298b265d
Parth Goel <parthgoel1204@gmail.com>
2026-03-07 14:32:14 +0530

    feat: added state-based variables in class-based component

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,10 +1,13 @@
+import {useState} from "react";
 interface UserProps {
   name: string;
 }
 
 const User = (props: UserProps) => {
+  const [count,setCount] = useState(0);
   return (
     <div className="user-card">
+      <h1>{count}</h1>
       <h2>{props.name}</h2>
       <h3>Location: Panipat</h3>
       <h4>Contact: @codeslayerTanj</h4>
--- src/components/UserClass.tsx
+++ src/components/UserClass.tsx
@@ -2,15 +2,25 @@ import React from "react";
 interface UserClassProps{
     name : string;
 }
-class UserClass extends React.Component<UserClassProps>{
+interface UserClassState {
+    count: number;
+    count2: number;
+}
+class UserClass extends React.Component<UserClassProps,UserClassState>{
     constructor (props : UserClassProps){
         super(props);
         console.log(props);
-        
+        this.state= {
+            count:0,
+            count2:2,
+        }
     }
     render(){
+        // const {count,count2} = this.state;
         return (
         <div className="user-card">
+            <h1>Count: {this.state.count}</h1>
+            <h1>Count: {this.state.count2}</h1>
             <h2>Name: {this.props.name}</h2>
             <h3>Location: Panipat</h3>
             <h4>Contact: @codeslayerTanj</h4>
